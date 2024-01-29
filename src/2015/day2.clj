0b0e78019bf64e6564d701b735c9bef6dde75234
Jim Grace <james.h.grace@gmail.com>
2024-01-29 12:42:55 -0500

    day2/part2

--- src/2015/day2.clj
+++ src/2015/day2.clj
@@ -7,6 +7,7 @@
 ;;Function:  f(l,w,h) = (2lw + 2wh + 2hl) + min(l,w,h)
 
 (def example "2x3x4\n1x2x3")
+(def puzzle (slurp "resources/2015/day2/puzzle"))
 
 (defn surface-area 
   [dimensions]
@@ -15,15 +16,30 @@
        (* 2 w h)
        (* 2 h l))))
 
+
+(defn ribbon-length
+  [dimensions]
+  (let [[x y z] (sort dimensions)]
+    (+(+ (+ x x)
+        (+ y y))
+      (* x y z))))
+
 (defn dimensions 
   [string]
   (map #(Integer. %) (str/split string #"x")))
 
-(->> example
+(->> puzzle
      str/split-lines
      (map dimensions)
      (map surface-area)
-     (reduce +))
+     (reduce +)
+     (println))
 
+(->> puzzle
+     str/split-lines
+     (map dimensions)
+     (map ribbon-length)
+     (reduce +)
+     (println))
 
-
+(ribbon-length(dimensions "2x3x4"))
