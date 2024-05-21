1096704b59772402a611cd1e72ffe041bbbee247
James Grace <james.h.grace@gmail.com>
2024-05-20 21:55:51 -0400

    day3 part 1

--- src/2015/day2.clj
+++ src/2015/day2.clj
@@ -1,30 +1,29 @@
-(ns aoc.2015.day2
-  (:require [clojure.string :as str]))
+(ns day2
+  (:require
+   [clojure.string :as str]))
 
 ;; Given dimensions of a box in Length, Width, and Height in the form LxWxH, find the surface area
 ;; of each box with the addition of the smallest dimension added as extra
 
 ;;Function:  f(l,w,h) = (2lw + 2wh + 2hl) + min(l,w,h)
 
-(def example "2x3x4\n1x2x3")
-(def puzzle (slurp "resources/2015/day2/puzzle"))
+(def puzzle (slurp "resources/day2.puzzle.input"))
 
-(defn surface-area 
+(defn surface-area
   [dimensions]
   (let [[l w h] (sort dimensions)]
     (+ (* 3 l w)
        (* 2 w h)
        (* 2 h l))))
 
-
 (defn ribbon-length
   [dimensions]
   (let [[x y z] (sort dimensions)]
-    (+(+ (+ x x)
-        (+ y y))
-      (* x y z))))
+    (+ (+ (+ x x)
+          (+ y y))
+       (* x y z))))
 
-(defn dimensions 
+(defn dimensions
   [string]
   (map #(Integer. %) (str/split string #"x")))
 
@@ -32,14 +31,11 @@
      str/split-lines
      (map dimensions)
      (map surface-area)
-     (reduce +)
-     (println))
+     (reduce +))
 
 (->> puzzle
      str/split-lines
      (map dimensions)
      (map ribbon-length)
-     (reduce +)
-     (println))
+     (reduce +))
 
-(ribbon-length(dimensions "2x3x4"))
---
+++ src/2015/day3.clj
@@ -0,0 +1,44 @@
+(ns day3)
+(def example1 \>)
+(def example2 "^>v<")
+(def example3 "v^v^v^v^")
+
+;;define a map translation of directional strings to directional vectors
+(def moves
+  {">" [1 0]
+   "^" [0 1]
+   "<" [-1 0]
+   "v" [0 -1]})
+
+;;convert the string input into directional vectors
+(defn parse-movements
+  "Given a string value of the directions a directional vector is returned"
+  [input]
+  (replace moves (map str (seq input))))
+
+(defn next-house
+  "x: the current location in form [[x y] #{x' y'}] where the first vec is last location and the set is the set of visited locations
+   y: the next directional vector.
+  Return: [[y] #{y x ...}]"
+  [x y]
+  (let [nxt (map + (first x) y)]
+    [nxt (conj (last x) nxt)]))
+
+;;Starting position to act as an accumulator for the "next-house" and "track-houses" functions
+;;the first element is the last visited location on a 2d grid
+;;the second element, the set, is the set of locations already visited. This will be and accumulator.
+(def start [[0 0] #{[0 0]}])
+
+(defn track-houses
+  "Given a seq of moves as vectors, returns a set of all visited houses."
+  [moves]
+  (last (reduce next-house start moves)))
+
+;;read in puzzle input
+(def puzzle (slurp "resources/day3.puzzle.input"))
+
+;; Part 1 Solution
+(->> puzzle
+     parse-movements
+     track-houses
+     count)
